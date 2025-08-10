bd24879bfcd467e4c2b63d7c984c0b9992252386
TKspayair <tkworking2003@gmail.com>
2025-08-10 14:46:01 +0900

    my-dictionary start success

---
+++ app/Models/Word.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Word extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'word_name', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'word_tag', 'word_id', 'tag_id');
+    }
+}
